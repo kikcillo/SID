0b4b1f1546b1f66bb67e0349bcb91481b99386b3
Bea95 <beamarand95@gmail.com>
2018-02-26 13:14:32 +0100

    cambios

--- src/CotizacionesHandler.java
+++ src/CotizacionesHandler.java
@@ -1,3 +1,5 @@
+package Practica1;
+
 import java.util.*;
 
 import org.xml.sax.*;
@@ -5,35 +7,33 @@ import org.xml.sax.helpers.*;
 
 public class CotizacionesHandler extends DefaultHandler {
 
-    private Hashtable<String, Double> tabla = new Hashtable<String, Double>();
+	private Hashtable<String, Double> tabla = new Hashtable<String, Double>();
 
-    //...
-    @Override
-    public void startDocument() throws SAXException {
-    }
+	// ...
+	@Override
+	public void startDocument() throws SAXException {
+	}
 
-    @Override
-    public void endDocument() throws SAXException {
-    }
+	@Override
+	public void endDocument() throws SAXException {
+	}
 
-    @Override
-    public void startElement(String uri, String localName, String qName,
-                             Attributes attributes) throws SAXException {
-//...
-    }
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		// ...
+	}
 
-    @Override
-    public void endElement(String uri, String localName, String qName)
-            throws SAXException {
-//...
-    }
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		// ...
+	}
 
-    @Override
-    public void characters(char ch[], int start, int length) throws SAXException {
-//...
-    }
+	@Override
+	public void characters(char ch[], int start, int length) throws SAXException {
+		// ...
+	}
 
-    public Hashtable<String, Double> getTabla() {
-        return tabla;
-    }
+	public Hashtable<String, Double> getTabla() {
+		return tabla;
+	}
 }
