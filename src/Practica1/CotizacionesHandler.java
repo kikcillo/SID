da0aac75c466c6caa9a0bdbc9a91d04ad76ceee6
Bea95 <beamarand95@gmail.com>
2018-03-06 14:49:31 +0100

    Cambio a UTF-8 e intento de escritura en fichero

--- src/Practica1/CotizacionesHandler.java
+++ src/Practica1/CotizacionesHandler.java
@@ -12,25 +12,27 @@ public class CotizacionesHandler extends DefaultHandler {
 	// ...
 	@Override
 	public void startDocument() throws SAXException {
+		System.out.println("Empieza el documento");
 	}
 
 	@Override
 	public void endDocument() throws SAXException {
+		System.out.println("Termina el documento");
 	}
 
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-		// ...
+		System.out.println("Leo una etiqueta");
 	}
 
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
-		// ...
+		System.out.println("Cierro una etiqueta");
 	}
 
 	@Override
 	public void characters(char ch[], int start, int length) throws SAXException {
-		// ...
+		System.out.println("Leo caracteres");
 	}
 
 	public Hashtable<String, Double> getTabla() {
